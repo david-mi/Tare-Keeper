bb73619d92d5496004864adbc75e67c0b799f173
David Michel <david.michel2809@gmail.com>
2025-06-13 08:26:51 +0200

    feat(_layout): add settings link on stack header

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,24 @@
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { useMMKVDevTools } from '@dev-plugins/react-native-mmkv';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { CustomButton } from "../components/shared/customButton/customButton";
 
 export default function RootLayout() {
   useMMKVDevTools();
   return (
     <GestureHandlerRootView>
-      <Stack screenOptions={{ title: "Récipients" }} />
+      <Stack screenOptions={{
+        title: "Récipients",
+        headerRight: () => (
+          <Link href="/settings" asChild>
+            <CustomButton
+              iconName="settings"
+              theme="circle"
+              onPress={() => console.log("Navigate to settings")}
+            />
+          </Link>
+        )
+      }} />
     </GestureHandlerRootView>
   );
 }
