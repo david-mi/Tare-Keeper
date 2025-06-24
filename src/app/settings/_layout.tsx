926168829a2e12b459eab048301039b7f993526a
David Michel <david.michel2809@gmail.com>
2025-06-24 14:19:06 +0200

    feat: add base logic to store foodContainers to a json file in the FileSystem

--- src/app/settings/_layout.tsx
+++ src/app/settings/_layout.tsx
@@ -4,6 +4,7 @@ export default function SettingsLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Paramètres" }} />
+      <Stack.Screen name="exportContainers" options={{ title: "Exporter les récipients" }} />
     </Stack>
   );
 }
---
+++ src/app/settings/exportContainers.tsx
@@ -0,0 +1,5 @@
+import { ExportContainers } from "@/src/components/settings/exportContainers/exportContainers";
+
+export default function ExportContainersPage() {
+  return <ExportContainers />;
+}
